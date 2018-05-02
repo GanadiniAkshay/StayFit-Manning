1e8bf2f839dd952d4ddec6918a05929a1d4ac50f
Akshay Kulkarni <akshaykulkarni.2104@gmail.com>
2018-05-02 20:55:04 +0530

    added code for chapter 2

--- chapters/ch-1/src/index.js
+++ chapters/ch-1/src/index.js
@@ -1,23 +1,23 @@
-import restify from 'restify'
-import * as builder from 'botbuilder'
+import restify from "restify"
+import * as builder from "botbuilder"
 
 // Setting up the server
 const server = restify.createServer()
 server.listen(3978, () => {
-  console.log('%s listening to %s', server.name, server.url)
+  console.log("%s listening to %s", server.name, server.url)
 })
 
 // Create chat bot
 const connector = new builder.ChatConnector({
-  appId: '',
-  appPassword: '',
+  appId: "",
+  appPassword: "",
 })
 
 const bot = new builder.UniversalBot(connector)
 
-server.post('/api/messages', connector.listen())
+server.post("/api/messages", connector.listen())
 
-bot.dialog('/', (session) => {
+bot.dialog("/", (session) => {
   // Edit more dialog here
-  session.send('You said: ' + session.message.text);
+  session.send("You said: " + session.message.text);
 })
